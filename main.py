f1367857d2beba0957bbc4e33a001ca8ddaa59d0
Jason <jason@duedil.com>
2014-06-19 21:11:15 +0000

    removing kruft

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-import pdb
 import json
 
-from giphypop  import Giphy
 from bottle import route, run, request
+from giphypop  import Giphy
 import requests
 
 giphy = Giphy()
 
-FUNCTIONS = ['random_gif']
-
 @route('/r', method=['POST', 'GET'])
 def r():
-    res = getattr(globals()['giphy'], 'random_gif')()
+    res = giphy.random_gif().next()
     gif_url = res['raw_data']['images']['original']['url']
     return json.dumps({"text": gif_url})
 
-@route('/g', method=['POST', 'GET'])
+@route('/g', method=['POST'])
 def g():
     search = request.forms.get('text')[3:]
     res = giphy.search(search).next()
-    #pdb.set_trace()
     gif_url = res['raw_data']['images']['original']['url']
     return json.dumps({"text": gif_url})
 
-run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    run(host="0.0.0.0", port=8080)
