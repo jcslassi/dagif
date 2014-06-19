e6b01722391c6c9429698e7cc5b57b310cd5bd2b
Jason <jason@duedil.com>
2014-06-19 21:30:48 +0000

    c̘̹̱̹̠o̹͍̟̱̱͜m̙p̢u͚s̡͔͍͔ḙ͕͡r̶̭̮̙̥̺vḛ̸̣̥͓̖̘

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ giphy = Giphy()
 
 @route('/r', method=['POST', 'GET'])
 def r():
-    res = giphy.random_gif().next()
+    res = giphy.random_gif()
     gif_url = res['raw_data']['images']['original']['url']
     return json.dumps({"text": gif_url})
 
 @route('/g', method=['POST'])
 def g():
     search = request.forms.get('text')[3:]
-    res = giphy.search(search).next()
+    """
+    iterations = 1
+    if search.endswith('.'):
+        iterations = search.count('.') + 1
+    for x in range(iterations):
+        print "iteration", x
+        import pdb; pdb.set_trace()
+        res = giphy.search(search.replace('.', '')).next()
+    """
+    res = giphy.search(search.replace('.', '')).next()
     gif_url = res['raw_data']['images']['original']['url']
     return json.dumps({"text": gif_url})
 
