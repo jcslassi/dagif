b295c73277a40558aabb909160cfd2458575a5c5
jason <jason@duedil.com>
2014-06-19 23:18:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+import logging
 
 from bottle import route, run, request
 from giphypop  import Giphy
 import requests
+
+ITERATIONS = False
 
 giphy = Giphy()
 
@@ -15,17 +18,18 @@ def r():
 @route('/g', method=['POST'])
 def g():
     search = request.forms.get('text')[3:]
-    """
-    iterations = 1
-    if search.endswith('.'):
-        iterations = search.count('.') + 1
-    for x in range(iterations):
-        print "iteration", x
-        import pdb; pdb.set_trace()
+    logging.info("Serving %s for user %s" % (search, request.forms.get('user_name'))
+    if ITERATIONS:
+        iterations = 1
+        if search.endswith('.'):
+            iterations = search.count('.') + 1
+        for x in range(iterations):
+            res = giphy.search(search.replace('.', '')).next()
+    else:
         res = giphy.search(search.replace('.', '')).next()
-    """
-    res = giphy.search(search.replace('.', '')).next()
+    
     gif_url = res['raw_data']['images']['original']['url']
+    
     return json.dumps({"text": gif_url})
 
 if __name__ == '__main__':
